f243eab8060980adfe0b201e2bfdd2ca44cd3d47
henrytranvn93 <henry.tran@htmi.ch>
2021-05-11 11:33:16 -0700

    final submission before inclass presentation

--- client/src/components/JobDetails/JobRoadMap.jsx
+++ client/src/components/JobDetails/JobRoadMap.jsx
@@ -9,6 +9,7 @@ export default function JobRoadMap() {
                 <div className="roadmap__texts">
                     <h4 className="roadmap__title">Coming Soon!</h4>
                     <p className="roadmap__notification">The feature helps you tracking each step of application process.</p>
+                    <a href="https://huy-tran-portfolio-364e1.web.app/" target="_blank" rel="noopener noreferrer">Contact me for more information</a>
                 </div>
             </div>
             
